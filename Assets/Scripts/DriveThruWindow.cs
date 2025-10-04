3351d3f276615fc31ad553b4d481dbb41d8f76f8
OctoAlgo <dodori.marc@googlemail.com>
2025-10-04 13:41:42 +0200

    Drive Through Window
    Opens and closes now, with Interaction Text

---
+++ Assets/Scripts/DriveThruWindow.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+
+public class DriveThruWindow : Interactable
+{
+
+    public bool isOpen = false;
+    KeyCode interactionKey;
+
+    public float openPositionX;
+    public float closedPositionX;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        GameObject player = GameObject.FindWithTag("Player");
+        interactionKey = player.GetComponent<PlayerInteraction>().interactionKey;
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void LookAt()
+    {
+        InteractText.Instance.ShowText(interactionPrompt);
+
+        if (isOpen)
+        {
+            this.interactionPrompt = $"Press '{interactionKey}' to close the drive-thru window.";
+        }
+        else
+        {
+            this.interactionPrompt = $"Press '{interactionKey}' to open the drive-thru window.";
+        }
+    }
+
+    public override void Interact()
+    {
+        MoveWindow();
+        base.Interact();
+    }
+
+    public void MoveWindow()
+    {
+        switch(isOpen)
+        {
+            case true:
+
+                // Is open, so close.
+                transform.localPosition = new Vector3(closedPositionX, transform.localPosition.y, transform.localPosition.z);
+                isOpen = false;
+
+                break;
+            case false:
+
+                // Is closed, so open.
+                transform.localPosition = new Vector3(openPositionX, transform.localPosition.y, transform.localPosition.z);
+                isOpen = true;  
+
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerInteraction.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour
+{
+    public float interactionRange;
+    public KeyCode interactionKey;
+    public Transform cameraObject;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Physics.Raycast(cameraObject.position, cameraObject.transform.forward, out RaycastHit hitInfo, interactionRange))
+        {
+            Interactable interactable = hitInfo.collider.GetComponent<Interactable>();
+            if(interactable != null)
+            {
+                interactable.LookAt();
+
+                if (Input.GetKeyDown(interactionKey))
+                {
+                    interactable.Interact();
+                    InteractText.NoTarget();
+                }
+
+            }
+            else
+            {
+                InteractText.NoTarget();
+            }
+        }
+    }
+}
