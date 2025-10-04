0c072df953ec375c1aeaeeea9fbbde3f631f226d
OctoAlgo <dodori.marc@googlemail.com>
2025-10-04 16:24:21 +0200

    Fiddled with Variable Jump Height a lot
    Let's talk about if it's shit or not lmao

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -17,8 +19,19 @@ public class PlayerMovement : MonoBehaviour
     [Header("Ground Check Settings")]
     public float playerHeight;
     public LayerMask groundLayer;
-    public float groundDistance = 0.4f;
+    public float groundDistance = 0.2f;
+    public float coyoteTime = 0.1f; // optional small grace window
     public bool grounded;
+    private bool wasGroundedLastFrame;
+    private bool hasLeftGround;
+    private float timeSinceGrounded;
+
+    [Header("Variable Jump Settings")]
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = .5f;
+    private float jumpTimeHeld;
+    public float minJumpTime = 0.1f;
+    private bool isJumping;
 
     public Transform orientation;
     public Transform groundCheck;
@@ -34,7 +47,7 @@ public class PlayerMovement : MonoBehaviour
         rb.freezeRotation = true;
 
         grounded = true;
-        canJump = true;
+        ResetJump();
     }
 
     public void Update()
@@ -42,6 +55,13 @@ public class PlayerMovement : MonoBehaviour
         //grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, groundLayer);
         grounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundLayer);
 
+        if (grounded)
+            timeSinceGrounded = 0f;
+        else
+            timeSinceGrounded += Time.deltaTime;
+
+        // === Reliable jump reset ===
+        if (grounded && rb.linearVelocity.y <= 0.1f) ResetJump();
 
         PlayerInput();
         SpeedControl();
@@ -50,6 +70,8 @@ public class PlayerMovement : MonoBehaviour
             rb.linearDamping = groundDrag;
         else
             rb.linearDamping = 0;
+
+        HandleVariableJump();
     }
 
     private void FixedUpdate()
@@ -62,13 +84,13 @@ public class PlayerMovement : MonoBehaviour
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
 
-        if (Input.GetKey(jumpKey) && canJump && grounded)
+        if (Input.GetKeyDown(jumpKey) && canJump && (grounded || timeSinceGrounded < coyoteTime))
         {
             canJump = false;
             Jump();
-            Invoke(nameof(ResetJump), jumpCooldown);
         }
     }
+
 
     private void MovePlayer()
     {
@@ -92,6 +114,10 @@ public class PlayerMovement : MonoBehaviour
     {
         rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
         rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+
+        isJumping = true;
+        jumpTimeHeld = 0f;
+
     }
 
     private void SpeedControl()
@@ -103,6 +129,32 @@ public class PlayerMovement : MonoBehaviour
             rb.linearVelocity = new Vector3(limitedVelocity.x, rb.linearVelocity.y, limitedVelocity.z);
         }
     }
+
+    private void HandleVariableJump()
+    {
+        if (isJumping)
+            jumpTimeHeld += Time.deltaTime;
+
+        if (rb.linearVelocity.y < 0)
+        {
+            rb.linearVelocity += Vector3.up * Physics.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+            isJumping = false;
+        }
+        else if (rb.linearVelocity.y > 0)
+        {
+            if (!Input.GetKey(jumpKey))
+            {
+                rb.linearVelocity += Vector3.up * Physics.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+            }
+        }
+    }
+
+    private IEnumerator JumpAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        canJump = true;
+    }
+
 
     private void ResetJump()
     {
