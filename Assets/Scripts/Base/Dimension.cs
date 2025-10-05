421417381861bd403d5981c76df0faf30871003b
OctoAlgo <dodori.marc@googlemail.com>
2025-10-05 18:28:42 +0200

    Revert "Merge branch 'main' of https://github.com/dodoriexe/Fast-Food-Dimensions"
    This reverts commit 03b27471b2afb38e484884725f5ad5262c188119, reversing
    changes made to 9ad21eaa8595c16c7a354ec9ce3f1bf6e4ddb31e.

--- Assets/Scripts/Base/Dimension.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "New Dimension", menuName = "Dimension")]
-public class Dimension : ScriptableObject
-{
-    public string dimensionName;
-    public Color dimensionColor;
-}
